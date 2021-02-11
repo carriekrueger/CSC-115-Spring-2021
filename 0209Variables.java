6ff1909b925b688533efd25988968a47c24372e0
carriekrueger <42945276+carriekrueger@users.noreply.github.com>
2021-02-11 09:50:31 -0500

    Update 0209Variables.java

--- 0209Variables.java
+++ 0209Variables.java
@@ -1,10 +1,12 @@
 // Carrie Krueger
-// 2-09-21
-// Notes about variables 
+// 2-9-21
+// Notes about variables and String output
 
 public class Variables {
     public static void main(String[] args){
         variables(); // a method to investigate variables
+        aboutStrings(); // a method to look at Strings 
+        
     }
     
     //in this method we will look at variables
@@ -56,9 +58,67 @@ public class Variables {
         gpa = 2.7;
         System.out.println("Your gpa is now " + gpa);
         
+        // In general... 
         
+        // int      num     =  28;
+        // type
+        //          name       
+        //                     value 
+        
+        
+        int num1 = 4; 
+        int num2 = 5;
+        
+        // you can do stuff with variables - print, math...
+        
+        int sum = num1 + num2;
+        System.out.println(sum); // 9
+        
+        int x;   // x has been DECLARED, but it has not been ASSIGNED
+                 // (needs to be initialized)
+        
+       // System.out.println(x);  // error! won't work
        
-      
+       // YOU TRY!
+       // 1. Add to PrintQuote.java or make a new class
+       // 2. Declare 2 int variables and assign them values
+       // 3. Dispay the sum 
+       // 4. Display the product
+        
         
     }
+    
+    
+    // this method looks at Strings
+    public static void aboutStrings() {
+        
+        // STRING
+        // A variable that hold a bunch of characters (like a word)
+        // String is a class (note the uppercase), but we can use
+        // it like a primitive 
+        
+        String firstName = "Carrie"; // note the camelCase 
+        System.out.println(firstName + " " + "Krueger"); //CONCATENATE
+    
+        // Mixing Strings and numbers is a tricky business...
+        
+        System.out.println("3 + 4");
+        System.out.println(3 + 4);
+        System.out.println(3 + 4 + " = 3 + 4");
+        System.out.println("3 + 4 = " + 3 + 4); // concatenates onto the String
+        System.out.println("3 + 4 = " + (3 + 4)); // add () for order of ops 
+        
+        System.out.println(3 - 4);
+        //System.out.println("3 - 4 = " + 3 - 4); // error!
+        System.out.println("3 - 4 = " + (3 - 4)); //order of ops
+        
+        int difference = 3 - 4; 
+        System.out.println("3 - 4 = " + difference);
+        
+    }
+    
+    
+    
+    
+    
 }
